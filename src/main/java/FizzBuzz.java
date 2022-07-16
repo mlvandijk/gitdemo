80a8e9380fb220eb6e241326acf7cb926db46a6c
marit.van.dijk <mlvandijk@gmail.com>
2022-07-16 09:45:39 +0200

    Fix formatting

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,14 +1,14 @@
 public class FizzBuzz {
 
     public String fizzBuzz(int input) {
-    StringBuilder sb = new StringBuilder();
-    if (input % 3 == 0) {
-        sb.append("Fizz");
-    }
-    if (input % 5 == 0) {
-        sb.append("Buzz");
-    }
-    return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        if (input % 3 == 0) {
+            sb.append("Fizz");
+        }
+        if (input % 5 == 0) {
+            sb.append("Buzz");
+        }
+        return sb.toString();
     }
 
 }
