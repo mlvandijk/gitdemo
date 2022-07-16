eabf0c8f33eff7e71850d56e320a98364f63b2bd
marit.van.dijk <mlvandijk@gmail.com>
2022-07-16 09:34:25 +0200

    Implement feature

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,11 @@
 public class FizzBuzz {
 
+
  public String fizzBuzz(int input) {
-     if (input % 3 == 0) {
+     if (input % 3 == 0 && input % 5 == 0){
+         return "FizzBuzz";
+
+     } else if (input % 3 == 0) {
          return "Fizz";
      } else if (input % 5 == 0) {
          return "Buzz";
