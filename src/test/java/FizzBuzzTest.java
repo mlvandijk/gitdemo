5b3469c81d04a40bb331a3f9d0ae2054603a9696
marit.van.dijk <mlvandijk@gmail.com>
2022-07-16 09:28:20 +0200

    Add failing test

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void ThreeShouldReturnFizz() {
+        FizzBuzz fb = new FizzBuzz();
+        String actual = fb.fizzBuzz(3);
+        assertEquals("Fizz", actual);
+    }
+}
