9ade5b34e9fd8f8e65de63c7dc20b7de648603c7
marit.van.dijk <mlvandijk@gmail.com>
2022-08-04 16:27:57 +0200

    Add field to Book

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -5,7 +5,8 @@ public record Book(String title,
                    List<String> author,
                    String publisher,
                    String isbn,
-                   int pages) {
+                   int pages,
+                   boolean illustrated) {
 
     public boolean containsAuthor(String name) {
         return author.contains(name);
