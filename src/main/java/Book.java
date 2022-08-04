17e0cd9b5fdc99604ed349c49b8edee45e8a4946
marit.van.dijk <mlvandijk@gmail.com>
2022-08-04 16:29:10 +0200

    Add publicationDate & preOrder check

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,13 +1,19 @@
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
 public record Book(String title,
                    List<String> author,
                    String publisher,
+                   LocalDate publicationDate,
                    String isbn,
                    int pages) {
 
     public boolean containsAuthor(String name) {
         return author.contains(name);
+    }
+
+    public boolean isPreOrder() {
+        return publicationDate.isAfter(LocalDate.now());
     }
 }
